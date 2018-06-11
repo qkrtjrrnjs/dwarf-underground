76b7d84c7e7a6e62a8bc436c39ce3eab8cd48148
Seok Kwun Park <park960@purdue.edu>
2018-06-11 15:03:35 -0400

    split page into 10 components

--- src/Clickbait.js
+++ src/Clickbait.js
@@ -4,7 +4,9 @@ class Clickbait extends Component{
     render(){
         return(
             <div className="small-12 columns other-articles">
+
                 <h2>From around the Realm</h2>
+                
                 <div className="small-6 medium-3 columns other-article">
                     <a href="#">
                         <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
@@ -29,6 +31,7 @@ class Clickbait extends Component{
                         <p>Surprise for Indiana Hobbits born before 1999</p>
                 </a> 
                 </div>
+
             </div>
         )
     }
