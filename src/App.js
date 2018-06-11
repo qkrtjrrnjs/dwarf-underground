966107a5daf84968cb9890be3036fc6315c21b0e
Seok Kwun Park <park960@purdue.edu>
2018-06-11 11:26:24 -0400

    basic structure, convert to components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header'
+import Aside from './Aside'
+import Article from './Article'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <body>
+          <Header />
+
+      <main className="expanded row">
+        <Article />
+        <Aside />
+
+      <div className="small-12 columns other-articles">
+        <h2>From around the Realm</h2>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
+            <p>Single Orcs in Indianapolis</p>
+          </a>  
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
+            <p>You won't believe what's under this mountain</p>
+          </a>  
+        </div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
+            <p>Mine 20% more gold with One Weird Trick</p>
+          </a> 
+        </div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
+            <p>Surprise for Indiana Hobbits born before 1999</p>
+          </a> 
+        </div>
+      </div>
+    </main>
+
+    <footer className="expanded row">
+      <h6>&copy; 2017 Erebor Industries</h6>
+      <h6 className="float-right">All rights reserved</h6>
+    </footer>
+
+    
+  </body>
+        
       </div>
     );
   }
